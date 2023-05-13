da8accce5aae84088ea72f5faa7d7cffe38707c6
mmlvgx <mmlvgx@gmail.com>
2023-05-14 00:28:49 +0300

    Discord Bot

---
+++ NodeBot/Program.cs
@@ -0,0 +1,18 @@
+﻿using NetCord;
+using NetCord.Gateway;
+
+DotNetEnv.Env.TraversePath().Load(); // Load Dotenv
+String token = DotNetEnv.Env.GetString("token"); // Get Token String
+
+GatewayClient client = new(new Token(TokenType.Bot, token), new GatewayClientConfiguration()
+{
+    Intents = GatewayIntents.GuildMessages | GatewayIntents.DirectMessages | GatewayIntents.MessageContent,
+});
+
+client.Log += message =>
+{
+    Console.WriteLine(message);
+    return default;
+};
+await client.StartAsync();
+await Task.Delay(-1);
